f3b9a639ac9d16eb29f1a9f96afdbd065e96670d
TommySanchez12 <tomyjoaqo@gmail.com>
2022-09-16 16:07:28 -0300

    Tommy
    Clases Combustible y Ciudad

---
+++ src/tp2laboratorio/Combustible.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2laboratorio;
+
+/**
+ *
+ * @author Tommy
+ */
+public class Combustible {
+    
+}
