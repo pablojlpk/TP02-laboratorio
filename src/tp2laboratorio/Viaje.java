2aa363d8d27e10b9c9801e48246a2e4d27f70da9
jimenaseitz <113646608+jimenaseitz@users.noreply.github.com>
2022-09-16 16:58:29 -0300

    jim

--- src/tp2laboratorio/Viaje.java
+++ src/tp2laboratorio/Viaje.java
@@ -11,17 +11,16 @@ package tp2laboratorio;
  */
 public class Viaje {
 
-    Ciudad origen;
-    Ciudad destino;
-    int distancia;
-    Vehiculo vehiculo;
-    int cantpeajes;
-    boolean estansobremismaruta;
+    private Ciudad origen;
+    private Ciudad destino;
+    private int distancia;
+    private Vehiculo vehiculo;
+    private int cantpeajes;
+    private boolean estansobremismaruta;
 
     public Viaje() {
     }
 
-    
     public Viaje(Ciudad origen, Ciudad destino, Vehiculo vehiculo, int cantpeajes) {
         this.origen = origen;
         this.destino = destino;
@@ -29,23 +28,39 @@ public class Viaje {
         this.cantpeajes = cantpeajes;
     }
 
-    
-    
     double calcularDistancia() {
+<<<<<<< HEAD
         
         return 0;
+=======
+        double dist = 0;
+        if (this.estansobremismaruta == true) {
+            dist = this.destino.getUbicadoEnKm() - this.origen.getUbicadoEnKm();
+        } else {
+            dist = distancia;
+        }
+        return dist;
+>>>>>>> eaa8500d14733c78d9bf646cdf557a3552ea09b3
     }
 
     double calcularCostoPeaje() {
-        return 0;
+
+        return (this.cantpeajes * 25);
     }
 
     Combustible calcularCostoCombustible(Combustible combustible) {
+        /* el consumo de combustible 
+      
+        autos=7 litros cada 100
+        camionetas 10 litros cada 100
+        camiones 12 litros cada 100;
+         */
         Combustible retorna;
         return retorna;
     }
 
     double calcularCostoTotal() {
-        return 0;
+
+       return costotal;
     }
 }
